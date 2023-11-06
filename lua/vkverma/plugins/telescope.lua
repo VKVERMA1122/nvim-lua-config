ea1e0d527a8216d20ad983ddf1205fc669145427
VKVERMA1122 <vaibhavkumarverma1@gmail.com>
2023-11-06 16:20:59 +0530

    some more updates

--- lua/vkverma/plugins/telescope.lua
+++ lua/vkverma/plugins/telescope.lua
@@ -31,7 +31,6 @@ return {
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")  -- find files within current working directory, respects .gitignore
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")   -- find string in current working directory as you type
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-    -- keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
     keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")   -- list available help tags
     keymap.set("n", "<leader>ft", "<cmd>Telescope colorscheme<cr>") -- list available colorscheme
     keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>")     -- list available keymaps
@@ -51,8 +50,6 @@ return {
         previewer = false,
       })
     end, { desc = '[/] Fuzzily search in current buffer]' })
-
-    -- telescope git commands (not on youtube nvim video)
     keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>")   -- list all git commits (use <cr> to checkout) ["gc" for git commits]
     keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
     keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>")  -- list git branches (use <cr> to checkout) ["gb" for git branch]
