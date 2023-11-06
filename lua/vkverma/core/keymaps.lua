1d262a2442d75451334a3019454ec2d9434d6ec7
VKVERMA1122 <vaibhavkumarverma1@gmail.com>
2023-11-06 11:08:59 +0530

    some ramdom updates

--- lua/vkverma/core/keymaps.lua
+++ lua/vkverma/core/keymaps.lua
@@ -7,8 +7,7 @@ function is_available(plugin)
   return lazy_config_avail and lazy_config.spec.plugins[plugin] ~= nil
 end
 
----------------------
--- General Keymaps -------------------
+-- General Keymaps
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
@@ -23,6 +22,7 @@ keymap.set("n", "x", '"_x')
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
+--lsp keymaps
 keymap.set("n", "<leader>gl", function() vim.diagnostic.open_float() end, { desc = "Open floating diagnostic message" })
 keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, { desc = "Go to previous diagnostic message" })
 keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, { desc = "Go to next diagnostic message" })
@@ -33,12 +33,11 @@ keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, { desc = 
 keymap.set("n", "<leader>ln", function() vim.lsp.buf.rename() end, { desc = "LSP Code Rename" })
 keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { desc = "LSP Signature Help" })
 keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, { desc = "LSP Buffer Format" })
-keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, { desc = "LSP Buffer Format" })
 
 --buffer navigation
-keymap.set("n","[b","<cmd>bnext<cr>",{desc="Switch to next buffer"})
-keymap.set("n","]b","<cmd>bprev<cr>",{desc="Switch to previous buffer"})
-keymap.set("n","'b","<cmd>bd<cr>",{desc="Switch to previous buffer"})
+keymap.set("n", "[b", "<cmd>bnext<cr>", { desc = "Switch to next buffer" })
+keymap.set("n", "]b", "<cmd>bprev<cr>", { desc = "Switch to previous buffer" })
+keymap.set("n", "'b", "<cmd>bd<cr>", { desc = "Switch close current buffer" })
 
 --terminal navigation
 keymap.set("n", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Terminal left window navigation" })
@@ -49,23 +48,21 @@ keymap.set("n", "|", "<cmd>vsplit<cr>", { desc = "Vertical Split" })
 keymap.set("n", "\\", "<cmd>split<cr>", { desc = "Horizontal Split" })
 
 -- GitSigns
-if is_available "gitsigns.nvim" then
-  keymap.set("n", "<leader>g", sections.g)
-  keymap.set("n", "]g", function() require("gitsigns").next_hunk() end, { desc = "Next Git hunk" })
-  keymap.set("n", "[g", function() require("gitsigns").prev_hunk() end, { desc = "Previous Git hunk" })
-  keymap.set("n", "<leader>gl", function() require("gitsigns").blame_line() end, { desc = "View Git blame" })
-  keymap.set("n", "<leader>gL", function() require("gitsigns").blame_line { full = true } end,
-    { desc = "View full Git blame" })
-  keymap.set("n", "<leader>gp", function() require("gitsigns").preview_hunk() end, { desc = "Preview Git hunk" })
-  keymap.set("n", "<leader>gh", function() require("gitsigns").reset_hunk() end, { desc = "Reset Git hunk" })
-  keymap.set("n", "<leader>gr", function() require("gitsigns").reset_buffer() end, { desc = "Reset Git buffer" })
-  keymap.set("n", "<leader>gs", function() require("gitsigns").stage_hunk() end, { desc = "Stage Git hunk" })
-  keymap.set("n", "<leader>gS", function() require("gitsigns").stage_buffer() end, { desc = "Stage Git buffer" })
-  keymap.set("n", "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, { desc = "Unstage Git hunk" })
-  keymap.set("n", "<leader>gd", function() require("gitsigns").diffthis() end, { desc = "View Git diff" })
-end
+-- keymap.set("n", "<leader>g", secteons.g)
+keymap.set("n", "]g", function() require("gitsigns").next_hunk() end, { desc = "Next Git hunk" })
+keymap.set("n", "[g", function() require("gitsigns").prev_hunk() end, { desc = "Previous Git hunk" })
+keymap.set("n", "<leader>gl", function() require("gitsigns").blame_line() end, { desc = "View Git blame" })
+keymap.set("n", "<leader>gL", function() require("gitsigns").blame_line { full = true } end,
+  { desc = "View full Git blame" })
+keymap.set("n", "<leader>gp", function() require("gitsigns").preview_hunk() end, { desc = "Preview Git hunk" })
+keymap.set("n", "<leader>gh", function() require("gitsigns").reset_hunk() end, { desc = "Reset Git hunk" })
+keymap.set("n", "<leader>gr", function() require("gitsigns").reset_buffer() end, { desc = "Reset Git buffer" })
+keymap.set("n", "<leader>gs", function() require("gitsigns").stage_hunk() end, { desc = "Stage Git hunk" })
+keymap.set("n", "<leader>gS", function() require("gitsigns").stage_buffer() end, { desc = "Stage Git buffer" })
+keymap.set("n", "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, { desc = "Unstage Git hunk" })
+keymap.set("n", "<leader>gd", function() require("gitsigns").diffthis() end, { desc = "View Git diff" })
 
--- if is_available "toggleterm.nvim" then
+--ToggleTerm 
 keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>",
   { desc = "ToggleTerm horizontal split" })
 keymap.set("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", { desc = "ToggleTerm vertical split" })
@@ -73,7 +70,6 @@ keymap.set("n", "<F7>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
 keymap.set("t", "<F7>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
 keymap.set("n", "<C-z>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" }) -- requires terminal that supports binding <C-'>
 keymap.set("t", "<C-z>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" }) -- requires terminal that supports binding <C-'>
--- end
 
 -- Smart Splits
 if is_available "smart-splits.nvim" then
@@ -95,3 +91,4 @@ else
   keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { desc = "Resize split left" })
   keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { desc = "Resize split right" })
 end
+
