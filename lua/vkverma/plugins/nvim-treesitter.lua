53bd4bf13174a1018d719b18f5a296f35877e06a
VKVERMA1122 <vaibhavkumarverma1@gmail.com>
2023-11-24 13:47:52 +0530

    removed unwanted packages

--- lua/vkverma/plugins/nvim-treesitter.lua
+++ lua/vkverma/plugins/nvim-treesitter.lua
@@ -23,26 +23,7 @@ return {
           enable = true,
         },
         -- ensure these language parsers are installed
-        ensure_installed = {
-          "json",
-          "javascript",
-          "typescript",
-          "tsx",
-          "yaml",
-          "html",
-          "css",
-          "prisma",
-          "markdown",
-          "markdown_inline",
-          "svelte",
-          "graphql",
-          "bash",
-          "lua",
-          "vim",
-          "dockerfile",
-          "gitignore",
-          "query",
-        },
+        ensure_installed = {},
         incremental_selection = {
           enable = true,
           keymaps = {
