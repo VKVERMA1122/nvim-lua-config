648bbaba720c3d8cf6a16b54179784c105e88d3f
VKVERMA1122 <vaibhavkumarverma1@gmail.com>
2023-12-06 17:48:23 +0530

    replace hop with flash,indent lines and git signs refactor

--- lua/vkverma/core/keymaps.lua
+++ lua/vkverma/core/keymaps.lua
@@ -45,20 +45,6 @@ keymap.set("n", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Terminal right window na
 keymap.set("n", "|", "<cmd>vsplit<cr>", { desc = "Vertical Split" })
 keymap.set("n", "\\", "<cmd>split<cr>", { desc = "Horizontal Split" })
 
--- GitSigns
--- keymap.set("n", "<leader>g", secteons.g)
-keymap.set("n", "]g", function() require("gitsigns").next_hunk() end, { desc = "Next Git hunk" })
-keymap.set("n", "[g", function() require("gitsigns").prev_hunk() end, { desc = "Previous Git hunk" })
-keymap.set("n", "<leader>gl", function() require("gitsigns").blame_line() end, { desc = "View Git blame" })
-keymap.set("n", "<leader>gL", function() require("gitsigns").blame_line { full = true } end,
-  { desc = "View full Git blame" })
-keymap.set("n", "<leader>gp", function() require("gitsigns").preview_hunk() end, { desc = "Preview Git hunk" })
-keymap.set("n", "<leader>gh", function() require("gitsigns").reset_hunk() end, { desc = "Reset Git hunk" })
-keymap.set("n", "<leader>gr", function() require("gitsigns").reset_buffer() end, { desc = "Reset Git buffer" })
-keymap.set("n", "<leader>gs", function() require("gitsigns").stage_hunk() end, { desc = "Stage Git hunk" })
-keymap.set("n", "<leader>gS", function() require("gitsigns").stage_buffer() end, { desc = "Stage Git buffer" })
-keymap.set("n", "<leader>gu", function() require("gitsigns").undo_stage_hunk() end, { desc = "Unstage Git hunk" })
-keymap.set("n", "<leader>gd", function() require("gitsigns").diffthis() end, { desc = "View Git diff" })
 
 --ToggleTerm
 keymap.set("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>",
@@ -91,4 +77,4 @@ else
 end
 
 --hop navigation
-keymap.set("n", "s", "<cmd>HopWord<cr>", { desc = "Hop word" })
+-- keymap.set("n", "s", "<cmd>HopWord<cr>", { desc = "Hop word" })
