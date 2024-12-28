0b667215c8cd1f191d131500f1d75027d2e273c6
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2024-12-28 16:41:41 +0530

    revert last change

--- lua/vkverma/plugins/bufferline.lua
+++ lua/vkverma/plugins/bufferline.lua
@@ -3,41 +3,24 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	event = "VeryLazy",
 	version = "*",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				mode = "tabs",
-				diagnostics = "nvim_lsp",
-				diagnostics_update_on_event = true,
-				buffer_close_icon = "󰅖",
-				modified_icon = "● ",
-				close_icon = " ",
-				left_trunc_marker = " ",
-				right_trunc_marker = " ",
-				offsets = {
-					{
-						filetype = "NvimTree",
-						text = "File Explorer",
-						text_align = "left",
-						separator = true,
-					},
+	opts = {
+		options = {
+			mode = "tabs",
+			diagnostics = "nvim_lsp",
+			diagnostics_update_on_event = true, -- use nvim's diagnostic handler
+			buffer_close_icon = "󰅖",
+			modified_icon = "● ",
+			close_icon = " ",
+			left_trunc_marker = " ",
+			right_trunc_marker = " ",
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					text_align = "left",
+					separator = true,
 				},
 			},
-		})
-		vim.cmd("set showtabline=0")
-		-- More robust method to handle Alpha
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-			pattern = "alpha",
-			callback = function()
-				vim.cmd("set showtabline=0")
-			end,
-		})
-
-		vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
-			pattern = "alpha",
-			callback = function()
-				vim.cmd("set showtabline=2")
-			end,
-		})
-	end,
+		},
+	},
 }
