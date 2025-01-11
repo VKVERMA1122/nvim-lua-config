1b63d7223cc4ba91c4a06d25cd1fed84c4690b74
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2025-01-11 09:50:36 +0530

    added load event for neogit

--- lua/vkverma/plugins/neogit.lua
+++ lua/vkverma/plugins/neogit.lua
@@ -2,10 +2,10 @@ return {
 	{
 		"NeogitOrg/neogit",
 		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
-			"nvim-telescope/telescope.nvim", -- optional
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
 		},
+		cmd = "Neogit", --Autoload on command
 		config = true,
 	},
 }
