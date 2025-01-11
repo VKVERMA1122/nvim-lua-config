dfde2efa2972759471e9f81458a304ce023c47f9
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2025-01-11 09:51:24 +0530

    Revert "added load event for neogit"
    This reverts commit 1b63d7223cc4ba91c4a06d25cd1fed84c4690b74.

--- lua/vkverma/plugins/neogit.lua
+++ lua/vkverma/plugins/neogit.lua
@@ -2,10 +2,10 @@ return {
 	{
 		"NeogitOrg/neogit",
 		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+			"nvim-telescope/telescope.nvim", -- optional
 		},
-		cmd = "Neogit", --Autoload on command
 		config = true,
 	},
 }
