853155a869c5883433d79887cc89e4672b025567
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2025-01-27 16:06:46 +0530

    some change in blink config for cmd

--- lua/vkverma/plugins/blink.lua
+++ lua/vkverma/plugins/blink.lua
@@ -17,13 +17,11 @@ return {
 				},
 			},
 			appearance = {
-				use_nvim_cmp_as_default = false,
+				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "normal",
 			},
 			completion = {
-				list = { selection = { preselect = true, auto_insert = true } },
 				accept = { auto_brackets = { enabled = true } },
-
 				documentation = {
 					auto_show = true,
 					auto_show_delay_ms = 250,
