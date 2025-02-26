68aecd3339e2f372612ba28fc07f218a9211b910
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2025-02-26 14:01:09 +0530

    change lualine theme to "auto"

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -80,7 +80,7 @@ return {
 
 		lualine.setup({
 			options = {
-				theme = "tokyonight",
+				theme = "auto",
 				globalstatus = vim.o.laststatus == 3,
 				disabled_filetypes = {
 					statusline = {
